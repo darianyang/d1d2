dd58a918352aa53ee3ea957f9d2f9c4966714f67
Darian Yang <dty7@login2.ls6.tacc.utexas.edu>
2024-03-17 20:06:11 -0500

    added succ agg datasets

--- 2kod/ls6-we/tt_dist_ss_v00/succ/agg_rst_file.sh
+++ 2kod/ls6-we/tt_dist_ss_v00/succ/agg_rst_file.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 # running cpptraj to create an aggregate ncdf file from all bstates
+
+CMD="parm ../common_files/m01_2kod_12A.prmtop \n"
+for file in bstates_reverse/*.rst; do
+    CMD="$CMD trajin $file \n"
+done
+CMD="$CMD trajout agg_succ.nc \n"
+
+echo -e $CMD > agg_succ.cpp
+
